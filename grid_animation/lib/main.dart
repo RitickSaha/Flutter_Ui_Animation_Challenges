5a7ad223bfc15cab27867aa6cc4b80e009135b4c
Ritick Saha <riticks123@gmail.com>
2021-07-24 15:10:09 +0530

    Final Change.

--- grid_animation/lib/main.dart
+++ grid_animation/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'data/dataset.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -22,92 +24,190 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late AnimationController animationController;
+  Widget animatedSwitcher = SizedBox();
+  late List<MovieData> movieList;
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    movieList = MovieDataList().moviewDataList;
+    animatedSwitcher = SizedBox();
+    super.initState();
+  }
 
-  void _incrementCounter() {
+  bool listed = false;
+  @optionalTypeArgs
+  void didChangeDependencies() {
+    switchWidget();
+    super.didChangeDependencies();
+  }
+
+  switchWidget() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (!listed) {
+        animatedSwitcher = Container(
+          key: ValueKey<bool>(listed),
+          child: Image.asset(
+            movieList[1].assets,
+            height: 200,
+            width: 140,
+            fit: BoxFit.fitHeight,
+          ),
+        );
+      } else {
+        animatedSwitcher = Container(
+          key: ValueKey<bool>(listed),
+          // margin: EdgeInsets.all(8),
+          child: Image.asset(
+            movieList.last.assets,
+            height: 200,
+            width: 140,
+            fit: BoxFit.fitHeight,
+          ),
+        );
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        color: Colors.black,
+        width: double.infinity,
+        height: double.infinity,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: InkWell(
+                onTap: () {
+                  setState(() {
+                    listed = !listed;
+
+                    listed
+                        ? animationController.forward()
+                        : animationController.reverse();
+                    switchWidget();
+                  });
+                },
+                child: AnimatedIcon(
+                  icon: AnimatedIcons.list_view,
+                  progress: animationController,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: SingleChildScrollView(
+                child: Stack(
+                  children: List.generate(
+                    30,
+                    (index) => AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      width: double.infinity,
+                      margin: EdgeInsets.only(
+                          top: !listed ? 600.0 * index : 200.0 * index),
+                      height: 200 * 3,
+                      child: Stack(
+                        alignment: Alignment.topLeft,
+                        fit: StackFit.loose,
+                        children: List.generate(
+                          3,
+                          (index) => AnimatedSwitcher(
+                              duration: Duration(
+                                  milliseconds: 200 + (100 * (2 - index))),
+                              transitionBuilder:
+                                  (Widget child, Animation<double> animation) {
+                                if (child.key == ValueKey(false)) {
+                                  return PositionedTransition(
+                                    rect: RelativeRectTween(
+                                      begin: RelativeRect.fromLTRB(
+                                        0,
+                                        200.0 * (2 - index) -
+                                            200.0 * (2 - index),
+                                        350 - ((300.0 * (2 - index))),
+                                        200.0 * (2 - index),
+                                      ),
+                                      end: RelativeRect.fromLTRB(
+                                        0,
+                                        200.0 * (2 - index),
+                                        350,
+                                        0,
+                                      ),
+                                    ).animate(animation),
+                                    child: FadeTransition(
+                                      opacity: animation,
+                                      child: child,
+                                    ),
+                                  );
+                                }
+                                // -200.0 * (2 - index)
+                                return PositionedTransition(
+                                  rect: RelativeRectTween(
+                                    end: RelativeRect.fromLTRB(
+                                      0,
+                                      200.0 * (2 - index) - 200.0 * (2 - index),
+                                      350 - ((300.0 * (2 - index))),
+                                      200.0 * (2 - index),
+                                    ),
+                                    begin: RelativeRect.fromLTRB(
+                                      0,
+                                      200.0 * (2 - index),
+                                      350,
+                                      0,
+                                    ),
+                                  ).animate(animation),
+                                  child: FadeTransition(
+                                    opacity: animation,
+                                    child: child,
+                                  ),
+                                );
+                              },
+                              child: animatedSwitcher,
+                              layoutBuilder: (Widget? currentChild,
+                                  List<Widget> previousChildren) {
+                                return Container(
+                                  height: 200.0 * (2 - index + 1),
+                                  alignment: Alignment.topLeft,
+                                  width: double.infinity,
+                                  child: Stack(
+                                    alignment: Alignment.topLeft,
+                                    fit: StackFit.expand,
+                                    children: <Widget>[
+                                      // ...previousChildren,
+                                      if (currentChild != null) currentChild,
+                                    ],
+                                  ),
+                                );
+                              }),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
