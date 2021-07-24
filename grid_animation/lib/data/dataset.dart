9f0f949d13154b58a21ba4eb2d6cb412b9879ec8
Ritick Saha <riticks123@gmail.com>
2021-07-24 14:47:20 +0530

    Dataset for the Animation components

--- grid_animation/lib/data/dataset.dart
+++ grid_animation/lib/data/dataset.dart
@@ -1,8 +1,3 @@
-//  127 Hours (2010)
-// R | 94 min | Biography, Drama
-
-//  7.5  Rate 82 Metascore
-// A mountain climber becomes trapped under a boulder while canyoneering alone near Moab, Utah and resorts to desperate measures in order to survive.
 class MovieData {
   final String name;
   final int time;
@@ -22,8 +17,10 @@ class MovieData {
 }
 
 class MovieDataList {
-  late List<MovieData> moviewDataList;
-
+  late List<MovieData> moviewDataList = [];
+  MovieDataList() {
+    generateData();
+  }
   List<MovieData> generateData() {
     moviewDataList.add(
       MovieData(
@@ -44,29 +41,7 @@ class MovieDataList {
         category: 'Carton, Drama',
         discription:
             'A rogue artificial intelligence kidnaps the son of famed basketball player LeBron James, who then has to work with Bugs Bunny to win a basketball game.',
-        assets: 'assets/a2.jpeg',
-      ),
-    );
-    moviewDataList.add(
-      MovieData(
-        name: 'Escape Room: Tournament of Champions',
-        time: 88,
-        score: 4.3,
-        category: 'Action, Adventure, Horror',
-        discription:
-            'Six people unwittingly find themselves locked in another series of escape rooms, slowly uncovering what they have in common to survive',
-        assets: 'assets/a3.jpeg',
-      ),
-    );
-    moviewDataList.add(
-      MovieData(
-        name: '127 Hours',
-        time: 94,
-        score: 7.5,
-        category: 'Biography, Drama',
-        discription:
-            'A mountain climber becomes trapped under a boulder while canyoneering alone near Moab, Utah and resorts to desperate measures in order to survive.',
-        assets: 'assets/a1.jpeg',
+        assets: 'assets/a2.jpg',
       ),
     );
     moviewDataList.add(
@@ -77,97 +52,10 @@ class MovieDataList {
         category: 'Carton, Drama',
         discription:
             'A rogue artificial intelligence kidnaps the son of famed basketball player LeBron James, who then has to work with Bugs Bunny to win a basketball game.',
-        assets: 'assets/a2.jpeg',
+        assets: 'assets/a3.jpg',
       ),
     );
-    moviewDataList.add(
-      MovieData(
-        name: 'Escape Room: Tournament of Champions',
-        time: 88,
-        score: 4.3,
-        category: 'Action, Adventure, Horror',
-        discription:
-            'Six people unwittingly find themselves locked in another series of escape rooms, slowly uncovering what they have in common to survive',
-        assets: 'assets/a3.jpeg',
-      ),
-    );
-    moviewDataList.add(
-      MovieData(
-        name: '127 Hours',
-        time: 94,
-        score: 7.5,
-        category: 'Biography, Drama',
-        discription:
-            'A mountain climber becomes trapped under a boulder while canyoneering alone near Moab, Utah and resorts to desperate measures in order to survive.',
-        assets: 'assets/a1.jpeg',
-      ),
-    );
-    moviewDataList.add(
-      MovieData(
-        name: 'Space Jam: A New Legacy',
-        time: 64,
-        score: 9.3,
-        category: 'Carton, Drama',
-        discription:
-            'A rogue artificial intelligence kidnaps the son of famed basketball player LeBron James, who then has to work with Bugs Bunny to win a basketball game.',
-        assets: 'assets/a2.jpeg',
-      ),
-    );
-    moviewDataList.add(
-      MovieData(
-        name: 'Escape Room: Tournament of Champions',
-        time: 88,
-        score: 4.3,
-        category: 'Action, Adventure, Horror',
-        discription:
-            'Six people unwittingly find themselves locked in another series of escape rooms, slowly uncovering what they have in common to survive',
-        assets: 'assets/a3.jpeg',
-      ),
-    );
-    moviewDataList.add(
-      MovieData(
-        name: '127 Hours',
-        time: 94,
-        score: 7.5,
-        category: 'Biography, Drama',
-        discription:
-            'A mountain climber becomes trapped under a boulder while canyoneering alone near Moab, Utah and resorts to desperate measures in order to survive.',
-        assets: 'assets/a1.jpeg',
-      ),
-    );
-    moviewDataList.add(
-      MovieData(
-        name: 'Space Jam: A New Legacy',
-        time: 64,
-        score: 9.3,
-        category: 'Carton, Drama',
-        discription:
-            'A rogue artificial intelligence kidnaps the son of famed basketball player LeBron James, who then has to work with Bugs Bunny to win a basketball game.',
-        assets: 'assets/a2.jpeg',
-      ),
-    );
-    moviewDataList.add(
-      MovieData(
-        name: 'Escape Room: Tournament of Champions',
-        time: 88,
-        score: 4.3,
-        category: 'Action, Adventure, Horror',
-        discription:
-            'Six people unwittingly find themselves locked in another series of escape rooms, slowly uncovering what they have in common to survive',
-        assets: 'assets/a3.jpeg',
-      ),
-    );
-    moviewDataList.add(
-      MovieData(
-        name: 'Black Widow',
-        time: 74,
-        score: 6.9,
-        category: 'Action, Adventure, Sci-Fi',
-        discription:
-            'Natasha Romanoff confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.',
-        assets: 'assets/a4.jpeg',
-      ),
-    );
+
     return moviewDataList;
   }
 }
